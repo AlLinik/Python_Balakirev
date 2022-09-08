baa27cf356093ff0fd4922a834976017cc8c39db
HP <terlin@mail.ru>
2022-09-09 00:04:16 +0300

    balakirev_09_09_2022_4

---
+++ 3. print_input_map/print_input_map_2.py
@@ -0,0 +1,8 @@
+# ВАРИАНТ 1
+
+a, b = map(int, input().split())
+print(a ** b)
+
+# ВАРИАНТ 2
+
+print(pow(*map(int, input().split())))
---
+++ 3. print_input_map/print_input_map_3.py
@@ -0,0 +1,8 @@
+# ВАРИАНТ 1
+
+a, b = map(float, input().split())
+print(a + b)
+
+# ВАРИАНТ 2
+
+print(sum(map(float, input().split())))
