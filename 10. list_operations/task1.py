f42832b4c18001290deb511e8f7cb88c891b65e8
HP <terlin@mail.ru>
2022-09-21 23:47:57 +0300

    balakirev_21_09_2022_4

---
+++ 10. list_operations/task1.py
@@ -0,0 +1,8 @@
+""" Вводятся три целых числа в одну строку через пробел. Сформируйте список lst, 
+хранящий эти значения в порядке их ввода. Результат выведите на экран, используя команду: print(lst)"""
+
+print(list(map(int, input().split())))
+
+print([int(x) for x in input().split()])
+
+print([i for i in map(int, input().split())])
