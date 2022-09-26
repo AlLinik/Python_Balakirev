7b399842c3373efdbacd899074acc8738521500c
HP <terlin@mail.ru>
2022-09-26 22:19:54 +0300

    balakirev_26_09_2022

---
+++ 12. methods_list/task7.py
@@ -0,0 +1,11 @@
+"""Вводятся оценки студента (числа от 2 до 5) в одну строку через пробел.
+Необходимо определить количество двоек и вывести это значение на экран."""
+
+print(input().split().count('2'))
+
+print(list(map(int, input().split())).count(2))
+
+print(input().count('2'))
+
+print(sum(char == '2' for char in input().split()))
+
