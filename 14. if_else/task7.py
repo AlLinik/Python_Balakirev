e441ee6279c192c3de18a4dec9efc3a1dd7b0feb
HP <terlin@mail.ru>
2022-09-30 08:38:03 +0300

    balakirev_30_09_2022

---
+++ 14. if_else/task7.py
@@ -0,0 +1,21 @@
+"""Вводится список городов в одну строку через пробел.
+Если в этом списке присутствует город Москва, то удалить его.
+Вывести на экран результирующий список в виде строки с городами через пробел."""
+
+lst = list(map(str, input().split()))
+for i in lst:
+    if 'Москва' in lst:
+        lst.remove('Москва')
+print(*lst)
+
+
+lst = input().split()
+if 'Москва' in lst:
+    lst.remove('Москва')
+print(*lst)
+
+print(input().replace('Москва ', ''))
+
+print(*[i for i in input().split() if i != 'Москва'])
+
+print(input().replace('Москва', ' ').replace('  ', '').strip())
